a1ccbd190955d843ac81d84898b2d7b44b1cdcd1
IYuliia <catnew2012@gmail.com>
2026-06-12 20:49:13 +0200

    added booking modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // scripts go here
+// ===== MODAL =====
+const modal = document.getElementById('bookingModal');
+const modalOverlay = document.getElementById('modalOverlay');
+const modalClose = document.getElementById('modalClose');
+
+const bookBtns = document.querySelectorAll('.book-online-trigger');
+
+bookBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        modal.classList.add('is-open');
+        // document.body.style.overflow = 'hidden';
+    });
+});
+
+modalOverlay.addEventListener('click', () => {
+    modal.classList.remove('is-open');
+    // document.body.style.overflow = '';
+});
+
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('is-open');
+    // document.body.style.overflow = '';
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        modal.classList.remove('is-open');
+        // document.body.style.overflow = '';
+    }
 });
